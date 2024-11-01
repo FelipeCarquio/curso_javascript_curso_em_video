a9d832853874487bad3a66cff3251acea8fd02a5
FelipeCarquio <148922884+FelipeCarquio@users.noreply.github.com>
2024-11-01 10:23:16 -0300

    finalizando ex015

--- aula12/ex015/script.js
+++ aula12/ex015/script.js
@@ -9,28 +9,37 @@ function verificador() {
         var rsex = document.getElementsByName('radsex')
         var idade = ano - resano.value
         var genero = ''
+        var foto = document.createElement('div')
+        foto.setAttribute('id', 'rosto')
         if (rsex[0].checked) {
             genero = 'Homem'
             if (idade >= 0 && idade < 11) {
-                res.innerHTML = `Você é um(a) menino de ${idade} anos.`
-                document.getElementById('rosto').style.width = '300px'
-                document.getElementById('rosto').style.height = '300px'
-                document.getElementById('rosto').style.background = 'url(imagens/menino.jpg) center center no-repeat'
-                document.getElementById('rosto').style.backgroundSize = 'cover'
-                document.getElementById('rosto').style.borderRadius = '50%'
-                document.getElementById('rosto').style.margin = 'auto'
+                foto.style.background = 'url(imagens/menino.jpg) center center  no-repeat'
             } else if (idade < 21) {
-                document.getElementById('rosto').style.width = '300px'
-                document.getElementById('rosto').style.height = '300px'
-                document.getElementById('rosto').style.background = 'url(imagens/jovem_h.jpg) center center no-repeat'
-                document.getElementById('rosto').style.backgroundSize = 'cover'
-                document.getElementById('rosto').style.borderRadius = '50%'
-                document.getElementById('rosto').style.margin = 'auto'
+                foto.style.background = 'url(imagens/jovem_h.jpg) center top no-repeat'
+            } else if (idade < 60) {
+                foto.style.background = 'url(imagens/homem.jpg) center top no-repeat'
+            } else {
+                foto.style.background = 'url(imagens/senhor.jpg) center center no-repeat'
+                foto.style.backgroundSize = 'contain'
             }
         } else {
             genero = 'Mulher'
+            if (idade >= 0 && idade < 11) {
+                foto.style.background = 'url(imagens/menina.jpg) center center  no-repeat'
+            } else if (idade < 21) {
+                foto.style.background = 'url(imagens/jovem_m.jpg) center top no-repeat'
+                foto.style.backgroundSize = 'contain'
+            } else if (idade < 60) {
+                foto.style.background = 'url(imagens/mulher.jpg) center top no-repeat'
+                foto.style.backgroundSize = 'contain'
+            } else {
+                foto.style.background = 'url(imagens/senhora.jpg) center center no-repeat'
+                foto.style.backgroundSize = 'contain'
+            }
         }
-        // res.innerHTML = `Você é um(a) ${genero} de ${idade} anos.`
+        res.innerHTML = `Você é um(a) ${genero} de ${idade} anos.`
+        res.appendChild(foto)
     }
 
 
